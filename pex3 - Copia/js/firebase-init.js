cf92c8bed8d2a77373ead1c420b6fef9c0434177
Pedro Rodrigues Salomão <160697967+PedroSalomaoR@users.noreply.github.com>
2025-05-25 13:53:55 -0300

    Delete pex3 - Copia directory

--- pex3 - Copia/js/firebase-init.js
+++
@@ -1,7 +0,0 @@
-import { app } from "./firebase-config.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
-
-export const auth = getAuth(app);
-
-export const db = getFirestore(app);
